35fc2e724c3f91f5f97afc0cead40038b213dfe1
Morgane <morganealves@aol.com>
2022-10-04 23:07:12 -0300

    Update theme scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './css/App.css';
+import './theme/App.css';
+import './theme/typography/fonts.css'
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Placeholder
-      </p>
+      <span className='stylized-font text-primary' style={{fontSize: "32px"}}>Simulador</span>
+      <i className="bi-upload"></i>
     </div>
   );
 }
